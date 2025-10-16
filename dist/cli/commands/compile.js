2dcb54d29e136c0270e398178a5048e7171c6332
Ishimoto Koji <koji.ishimoto@gmail.com>
2025-10-16 22:23:46 +0900

    improve cli

--- dist/cli/commands/compile.js
+++ dist/cli/commands/compile.js
@@ -2,16 +2,12 @@ import React from 'react';
 import { Text, Box } from 'ink';
 import zod from 'zod';
 import { createRequire } from 'module';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import * as Parser from '#parser';
+import * as Compiler from '#compiler';
+import * as Ast from "#ast";
+import * as ANF from "#anf";
+import * as ClosureConversion from "#closure-conversion";
 const require = createRequire(import.meta.url);
-// When running from dist/cli/commands, go up 3 levels to project root, then into src/Lambda
-// @ts-ignore
-const Parser = require(resolve(__dirname, '../../../src/Lambda/Parser.bs.mjs'));
-// @ts-ignore
-const Compiler = require(resolve(__dirname, '../../../src/Lambda/Compile.bs.mjs'));
 export const options = zod.object({
     phase: zod
         .number()
@@ -30,25 +26,15 @@ export default function Compile({ options, args }) {
     try {
         const ast = Parser.parse(expression);
         if (options.ast) {
-            // @ts-ignore
-            const Ast = require(resolve(__dirname, '../../../src/Lambda/Ast.bs.mjs'));
             return (React.createElement(Box, { flexDirection: "column" },
                 React.createElement(Text, { color: "green" }, "AST:"),
                 React.createElement(Text, null, Ast.printLam(ast))));
         }
         if (options.anf) {
-            // @ts-ignore
-            const ANF = require(resolve(__dirname, '../../../src/Lambda/ANF.bs.mjs'));
-            // @ts-ignore
-            const ClosureConversion = require(resolve(__dirname, '../../../src/Lambda/ClosureConversion.bs.mjs'));
-            // @ts-ignore
-            const Ast = require(resolve(__dirname, '../../../src/Lambda/Ast.bs.mjs'));
-            // @ts-ignore
-            const Compile = require(resolve(__dirname, '../../../src/Lambda/Compile.bs.mjs'));
             const renamed = Ast.rename(ast);
             const anf = ANF.convert(renamed);
             const closure = ClosureConversion.convert(anf);
-            const hoisted = Compile.Hoisting.hoist(closure);
+            const hoisted = Compiler.Hoisting.hoist(closure);
             return (React.createElement(Box, { flexDirection: "column" },
                 React.createElement(Text, { color: "green" }, "ANF:"),
                 React.createElement(Text, null, ANF.printANF(hoisted))));
